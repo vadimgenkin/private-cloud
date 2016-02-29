b110cd3f6d6b1f9e3c19ac8cbddf433c7f5adb53
Vadim Genkin <vadim@vagenkin.corp.microsoft.com>
2016-02-29 08:21:44 -0500

    returning 400 if path is not present in call to ls

--- Application/routes.js
+++ Application/routes.js
@@ -1,8 +1,17 @@
 var fileserver = require('./fileserver');
 var fs = require('fs');
 
+var check_path = function (req, res) {
+	if(!req.body.path) {
+      res.status(400).send('Path is not defined');
+      return false;
+   } else {
+      return true;
+   }
+}
+
 exports.stat = function (req,res){
-	if(!req.body.path) res.send('Path is not defined');
+	if(!check_path) return;
 
 	fileserver.stat(req.body.path, function(err,stats){
 		if(stats){
@@ -16,7 +25,7 @@ exports.stat = function (req,res){
 
 //list directory
 exports.ls = function(req,res){
-	if(!req.body.path) return res.send('Path is not defined');
+	if(!check_path) return;
 
 	fileserver.ls(req.body.path, function(err, nodes){
 		if(!err){
@@ -43,7 +52,7 @@ exports.uploadresumable = function(req,res){
 }
 
 exports.chdir = function(req, res){
-	if(!req.body.path) return res.send('Path is not defined');
+	if(!check_path) return;
 
 	fileserver.chdir(req.body.path, function(err){
 		if(err) res.send({ error : err });
@@ -59,7 +68,8 @@ exports.memory = function(req,res){
 
 //get disk space
 exports.diskspace = function(req,res){
-	if(!req.body.path) return res.send('Path is not defined');
+	if(!check_path) return;
+
 	fileserver.diskspace(req.body.path, function(err, space){
 		if(!err){
 			res.json(space);
@@ -72,7 +82,7 @@ exports.diskspace = function(req,res){
 
 //delete file or folder
 exports.delete = function(req,res){
-	if(!req.body.path) return res.send('Path is not defined');
+	if(!check_path) return;
 
 	fileserver.delete(req.body.path, function(err, status){
 		if(!err) res.send(status);
@@ -82,7 +92,7 @@ exports.delete = function(req,res){
 
 //create new directory
 exports.createDir = function(req,res){
-	if(!req.body.path) return res.send('Path is not defined');
+	if(!check_path) return;
 
 	fileserver.createDir(req.body.path, function(err){
 		if(err) res.send({ error : err });
